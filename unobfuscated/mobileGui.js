3104036293be4ab393c4f7d0bedbee588d87ce14
iamcubeman12 <111822044+iamcubeman12@users.noreply.github.com>
2023-01-17 15:20:35 -0600

    Update mobileGui.js

--- unobfuscated/mobileGui.js
+++ unobfuscated/mobileGui.js
@@ -1,17 +1,3 @@
-/**
-* @license StewartPrivateLicense-2.0.0
-* Copyright (c) Aerell McKnight 2023
-*
-* You may not reproduce or distribute any code inside this file without the licenser's permission.
-* You may not copy, modify, steal, skid, or recreate any of the code inside this file.
-* You may not under any circumstance republish any code from this file as your own.
-* 
-* ALL TERMS STATED IN THE LINK BELOW APPLY ASWELL
-* https://github.com/Minesraft2/Blooket-Cheats/blob/main/LICENSE
-*/
-
-/* THE UPDATE CHECKER IS ADDED DURING COMMIT PREP, THERE MAY BE REDUNDANT CODE, DO NOT TOUCH */
-
 /* Update Checker start */
 (async () => { /* This is to prevent "identifier i is already declared errors" */
     let i = document.createElement('iframe');
@@ -19,7 +5,7 @@
     window.confirm = i.contentWindow.confirm.bind(window);
     i.remove();
     Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host : "play.blooket.com") + "/api/games?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/global/useAnyBlook.js")?.answers?.[0]}`)).then(async x => {
-        if (x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
+        if (x || confirm("This cheat is outdated and might be bugged, would you still like to run it?")) {
             /* Update Checker end */
             ; (() => {
                 let n = document.createElement('iframe');
@@ -143,7 +129,7 @@
                 bodyDiv.appendChild(footer);
                 footer.style.fontSize = '0.9rem';
                 footer.style.paddingBottom = '5px';
-                footer.innerHTML = (`<span><a target="blank" href="https://discord.gg/QerPBatcca">Made by OneMinesraft2#5394</a></span>`);
+                footer.innerHTML = (`<span><a target="blank" href="https://www.youtube.com/channel/UCA4_pJmSIajGNb8kBvbz70Q">Made by PizzaBoy#7871</a></span>`);
             
                 let cheats = ({
                     global: [
